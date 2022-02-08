908008542d48fb7f4c9c5077d67a93e405ff59d1
ruslan3220 <arifjanovruslan46@gmail.com>
2022-02-08 20:45:20 +0500

    initial commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// const elGamburger = document.querySelector(".header__gamburger");
+// // const elCros = document.querySelector("header__cros");
+// const elHeader = document.querySelector(".header");
+
+// elGamburger.addEventListener("click", function () {
+//   elHeader.classList.toggle("header__opened");
+// }
